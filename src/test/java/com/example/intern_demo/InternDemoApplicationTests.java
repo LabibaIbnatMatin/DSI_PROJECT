ee1d8bc641fc0da8d977fe5f3d9f56f6ef699d05
Labiba Ibnat Matin <ibnatmatin30@gmail.com>
2026-05-18 11:53:14 +0600

    fixed some errors

--- src/test/java/com/example/intern_demo/InternDemoApplicationTests.java
+++ src/test/java/com/example/intern_demo/InternDemoApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.intern_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.dsitask1.DSI_task1;
 
-@SpringBootTest
+@SpringBootTest(classes = DSI_task1.class)
 class InternDemoApplicationTests {
 
 	@Test
